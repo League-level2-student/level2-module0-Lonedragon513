0ef29f7576ea3285129fcad9b4817f08b26d2f2d
audreychan <league@wts14-4.attlocal.net>
2019-03-10 12:51:42 -0700

    01 and 02

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -6,48 +6,50 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class _01_RobotRace {
 	// 1. make a main method
-public static void main(String[] args) {
-	Robot[] r = new Robot [5];
-	
-	for (int i = 0; i < 5; i++) {
-		r[i] = new Robot();
-		r[i].setX(800-(i*50));
-		r[i].setY(600);
-		r[i].setSpeed(1000);
+	public static void main(String[] args) {
+		Robot[] r = new Robot[5];
+
+		for (int i = 0; i < 5; i++) {
+			r[i] = new Robot();
+			r[i].setX(800 - (i * 50));
+			r[i].setY(600);
+			r[i].setSpeed(1000);
 		}
-	Random ra = new Random ();
-	
-	
-	
-	//for (int i = 0; i > -5; i++) {
-		
-	
-+
-	for (int b = 0; b < 5; b++) {
-			System.out.println(b);
-		int a = ra.nextInt(500);
-		r[b].move(a);
-		
-		
+		Random ra = new Random();
+		boolean bb = false;
+		// for (int i = 0; i > -5; i++) {
+
+		while (bb == false) {
+
+			for (int b = 0; b < 5; b++) {
+				int a = ra.nextInt(10);
+				r[b].move(a);
+
+				if (r[b].getY() < 0) {
+					bb = true;
+					System.out.println(b);
+					// break;
+				}
+
+			}
 		}
-	
 	}
-//	}
+	// }
 }
-	//2. create an array of 5 robots.
+// 2. create an array of 5 robots.
 
-	//3. use a for loop to initialize the robots.
+// 3. use a for loop to initialize the robots.
 
-		//4. make each robot start at the bottom of the screen, side by side, facing up
+// 4. make each robot start at the bottom of the screen, side by side, facing up
 
-	//5. use another for loop to iterate through the array and make each robot move 
-	//   a random amount less than 50.
+// 5. use another for loop to iterate through the array and make each robot move
+// a random amount less than 50.
 
-	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
+// 6. use a while loop to repeat step 5 until a robot has reached the top of the
+// screen.
 
-	//7. declare that robot the winner and throw it a party!
+// 7. declare that robot the winner and throw it a party!
 
-	//8. try different races with different amounts of robots.
+// 8. try different races with different amounts of robots.
 
-	//9. make the robots race around a circular track.
-}
+// 9. make the robots race around a circular track.
