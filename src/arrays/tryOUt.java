5e555853d33924ab0e23ce6ec71878af2966db43
league <league@wts-im-17.attlocal.net>
2019-03-16 17:30:16 -0700

    1

---
+++ src/arrays/tryOUt.java
@@ -0,0 +1,16 @@
+package arrays;
+
+public class tryOUt {
+private void mian() {
+	// TODO Auto-generated method stub
+	int[] arr = new int[6];
+	arr[0] = 5;
+	arr[1] = 3;
+	arr[2] = 9;
+	arr[3] = 0;
+	arr[4] = 5;
+	System.out.println(arr[2]);
+
+	arr.reverPrint();
+}
+}
